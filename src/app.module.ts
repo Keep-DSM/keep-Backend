04095fd6ecea6ef1cda313e597b312e114e90486
Tyler <tylereunholee010203@gmail.com>
2025-03-25 10:33:39 +0900

    feat :: [#1] App Module 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { PresentationModule } from './presentation/presentation.module';
+import { GlobalConfigModule } from './common/module/config.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmConfigService } from './common/config/database.config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true
+        }),
+        TypeOrmModule.forRootAsync({
+            imports: [ConfigModule],
+            useClass: TypeOrmConfigService
+        }),
+        GlobalConfigModule,
+        PresentationModule
+    ]
 })
 export class AppModule {}
